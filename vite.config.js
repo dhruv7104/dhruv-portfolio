878d2117e0abc494989e037c536c21a731bb5043
dhruv7104 <dhruvgonnade000@gmail.com>
2025-07-13 22:36:50 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    // Optional: Allow access from Vercel's preview domain
-    allowedHosts: 'all'
-  },
-  build: {
-    outDir: 'dist',
-  },
-  // Optional: For custom domain or GitHub Pages base path
-  // base: '/your-repo-name/' 
-});
+  base: './', // important for Vercel or GitHub Pages
+})
